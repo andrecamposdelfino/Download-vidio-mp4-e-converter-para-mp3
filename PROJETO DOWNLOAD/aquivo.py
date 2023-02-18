0f7a97c5eb5395b4fa7cacbf5458c18373eb8d2a
@andrecampos <andre321.ac@gmail.com>
2023-02-18 11:58:16 -0300

    Add files via upload

---
+++ PROJETO DOWNLOAD/aquivo.py
@@ -0,0 +1,27 @@
+# from moviepy.editor import *
+#
+# # def converter_mp4():
+# #    try:
+# #        mp4_file = "Giveon - Favorite Mistake (Audio).mp4"
+# #        mp3_file = "Giveon - Favorite Mistake (Audio).mp3"
+# #        videoClip = VideoFileClip(mp4_file)
+# #        audioClip = videoClip.audio
+# #        audioClip.write_audiofile(mp3_file)
+# #
+# #        videoClip.close()
+# #        audioClip.close()
+# #    except Exception as erro:
+# #        print(f"Erro ao tentar converter erro : {erro}")
+# #
+# #
+# # converter_mp4()
+
+
+
+
+
+
+
+
+
+
